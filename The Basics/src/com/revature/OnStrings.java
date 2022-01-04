6f9181565b35b712a8d577968f2d7fdbb7942539
unknown <mich.walentynowicz@gmail.com>
2022-01-04 17:32:28 +0100

    1

---
+++ The Basics/src/com/revature/OnStrings.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+public class OnStrings {
+
+	public void showCoolFeature() {
+		
+		char[] charName = {'J', 'a', 'c', 'o', 'b'};
+		
+		String s = new String(charName);
+		
+		char [] newArray = s.toCharArray();
+	}
+}
